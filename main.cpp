39a9a1029008f221015e99a5a57d778432b027c9
Dmitry (ShepelevDD) <shepelev.mit@yandex.ru>
2020-03-13 12:17:25 +0300

    вывод максимума

--- main.cpp
+++ main.cpp
@@ -11,5 +11,10 @@ int main()
          << "A - B = "<< a - b << '\n'
          << "A * B = "<< a * b << '\n'
          << "A / B = "<< a / b << '\n';
+         if (a > b)
+         {
+             cout<<"max = "<< a;
+         }
+         else cout<<"max = "<<b;
     return 0;
 }
